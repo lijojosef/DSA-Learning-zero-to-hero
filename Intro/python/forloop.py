9f572ceb9148a302ef9b69a2f0dd9f0d3fcdbe20
Lijo Joseph <lijojoseph234@gmail.com>
2025-05-20 05:05:13 +0530

    adding basic for loop syntax

---
+++ Intro/python/forloop.py
@@ -0,0 +1,18 @@
+def main():
+    fruits = ["apples", "bananas", "managoes"]
+
+    for fruit in fruits:
+        print(fruit, end = " ")
+
+    print()
+
+    for i in range(5):
+        print(i, end = " ")
+
+    print()
+
+    for char in "python":
+        print(char, end = " ")
+
+if __name__ == "__main__":
+    main()
